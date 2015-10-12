cdfcf3ff16fb99cfa66ecda5eae5a8c378c72856
ad <adl@dimo-gestion.fr>
2015-10-12 08:29:16 +0200

    UPDATED

--- ESDEV/EServices/vendor/composer/autoload_namespaces.php
+++ ESDEV/EServices/vendor/composer/autoload_namespaces.php
@@ -6,14 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'XML_Util' => array($vendorDir . '/pear/xml_util'),
     'Ubench' => array($vendorDir . '/devster/ubench/src'),
-    'Structures' => array($vendorDir . '/pear/structures_graph'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log'),
     'Pimple' => array($vendorDir . '/pimple/pimple/lib'),
-    'PEAR' => array($vendorDir . '/pear/pear_exception'),
-    'Mail' => array($vendorDir . '/pear/mail', $vendorDir . '/pear/mail_mime'),
-    'Console' => array($vendorDir . '/pear/console_getopt'),
-    'Archive_Tar' => array($vendorDir . '/pear/archive_tar'),
-    '' => array($vendorDir . '/pear/pear-core-minimal/src'),
 );
--- ESDEV/EServices/vendor/composer/autoload_real.php
+++ ESDEV/EServices/vendor/composer/autoload_real.php
@@ -23,10 +23,6 @@ class ComposerAutoloaderInit34c19482c16a13d5ec19be98aa33c17d
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit34c19482c16a13d5ec19be98aa33c17d', 'loadClassLoader'));
 
-        $includePaths = require __DIR__ . '/include_paths.php';
-        array_push($includePaths, get_include_path());
-        set_include_path(join(PATH_SEPARATOR, $includePaths));
-
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
